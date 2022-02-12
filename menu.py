6b18e6ee4d25fd543768df0b1321cd7b45afc451
Pofka <45260470+pofka94@users.noreply.github.com>
2022-02-12 18:54:29 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,30 @@
 import pygame, os
 from config import *
+# import main
 
-class Menu:
-    def __init__(self):
-        self.screen = pygame.display.set_mode(DISPLAY)
+pygame.init()
+pygame.font.init()
+pygame.display.set_caption("Menu")
+clock = pygame.time.Clock()
 
-        running = True
+grey = (100, 100, 100)
 
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+def main():
+    screen = pygame.display.set_mode(DISPLAY)
+    screen.fill(grey)
+    font = pygame.font.SysFont("Arial", 50)
+    img = font.render('start', True, (255, 255, 255))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
                 
+        screen.blit(img, (20, 20))
+
+        pygame.display.update()
+        clock.tick(30)
+    pygame.quit()
+
+main()
