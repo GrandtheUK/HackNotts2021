46e00c5f82a7f1023eeb00aff55fc8b04f066691
Pofka <45260470+pofka94@users.noreply.github.com>
2022-02-12 14:27:18 +0000

    Finished setWeight in Fish class

--- fish.py
+++ fish.py
@@ -10,18 +10,25 @@ class Fish:
     def setWeight(self):
         if self.type == "Carp":
             return round(random.uniform(0.5, 4.0), 2)
-        if self.type == "Chub":
+        elif self.type == "Chub":
             return round(random.uniform(0.5, 8.0), 2)
-        if self.type == "Zander":
+        elif self.type == "Zander":
             return round(random.uniform(1.0, 11.0), 2)
-        if self.type == "Brown Trout":
+        elif self.type == "Brown Trout":
             return round(random.uniform(3.0, 20.0), 2)
-        if self.type == "Grayling":
-            return round(random.random(1.0, 6.7), 2)
-        if self.type == "Shoe fish":
+        elif self.type == "Grayling":
+            return round(random.uniform(1.0, 6.7), 2)
+        elif self.type == "Shoe fish":
             return random.randint(4, 13)
-        if self.type == "Dead fish":
-            return round(random.random(0.5, 10.0), 2)
+        elif self.type == "Dead fish":
+            return round(random.uniform(0.5, 10.0), 2)
+        elif self.type == "Magikarp":
+            return round(random.uniform(1.0, 10.0), 2)
+        elif self.type == "Nemo":
+            return round(random.uniform(3.0, 15.0), 2)
+        else:
+            return 1
+        
 
     
     def size(self, size):
@@ -31,15 +38,15 @@ class Fish:
     def getType(self):
         prob = random.random()
 
-        if prob < 0.5: return "Carp"
-        elif prob < 0.40: return "Chub"
-        elif prob < 0.35: return "Zander"
-        elif prob < 0.30: return "Brown Trout"
-        elif prob < 0.25: return "Grayling"
-        elif prob < 0.20: return "Shoe fish"
+        if prob < 0.05: return "Nemo"
+        elif prob < 0.10: return "Magikarp"
         elif prob < 0.15: return "Dead fish"
-        elif prob < 0.10: return "Tiny fin"
-        elif prob < 0.05: return "Nemo"
+        elif prob < 0.20: return "Shoe fish"
+        elif prob < 0.25: return "Grayling"
+        elif prob < 0.30: return "Brown Trout"
+        elif prob < 0.35: return "Zander"
+        elif prob < 0.40: return "Chub"
+        elif prob < 0.5: return "Carp"
         else:
             num = random.randint(1, 4)
 
