62a97f102eda805a2013726af72437b956991b21
Michael Swann <michaelswann.se@gmail.com>
2022-02-12 14:05:57 +0000

    modify main display tiles

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-import pygame, sys, level
+import pygame, sys, level, classes
 
 TILESIZE = 64
-
-def drawTestGrid(screen,gridSize,TILESIZE):
-    """Draws a grey and black grid to the screen"""
-    background=pygame.Surface(screen.get_size())
-    blackTile=pygame.Surface((TILESIZE, TILESIZE))
-    greyTile=pygame.Surface((TILESIZE, TILESIZE))
-    blackTile.fill((0,0,0))
-    blackTile.fill((128,128,128))
-
-    i=0
-    j=0
-    while i<gridSize[0]:
-        while j<gridSize[1]:
-            if (i%2==0) and (j%2==0):
-                background.blit(blackTile,(i*TILESIZE,j*TILESIZE))
-            elif (i%2==0) and (j%2==1):
-                background.blit(greyTile,(i*TILESIZE,j*TILESIZE))
-            elif (i%2==1) and (j%2==0):
-                background.blit(greyTile,(i*TILESIZE,j*TILESIZE))
-            elif (i%2==1) and (j%2==1):
-                background.blit(blackTile,(i*TILESIZE,j*TILESIZE))
-            j+=1
-        i+=1
-        j=0
-    screen.blit(background,(0,0))
 
 
 def main():
@@ -35,17 +10,14 @@ def main():
     screen = pygame.display.set_mode(DISPLAY)
     pygame.display.set_caption("Fishing Game")
 
-    screenSize=screen.get_size()
-    gridSize=[screenSize[0]/TILESIZE,screenSize[1]/TILESIZE]
+    tileGroup = pygame.sprite.Group()
+    thisLevel = level.getLevelData("01-river")
 
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((250,250,250))
-    drawTestGrid(screen,gridSize,TILESIZE)
-
-    screen.blit(background, (0,0))
-    drawTestGrid(screen,gridSize,TILESIZE)
-    pygame.display.flip()
+    for i in range(12):
+        for j in range(9):
+            id = thisLevel[j][i]
+            tile = classes.Tile(id,i*TILESIZE,j*TILESIZE,TILESIZE,TILESIZE)
+            tileGroup.add(tile)
 
 
     while True:
@@ -54,7 +26,7 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-
+        tileGroup.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
