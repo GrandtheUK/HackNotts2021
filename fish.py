0b45007a71cb01345b7d4c258b9346f34b942048
Pofka <45260470+pofka94@users.noreply.github.com>
2022-02-12 14:12:43 +0000

    Bruh

--- fish.py
+++ fish.py
@@ -7,9 +7,22 @@ class Fish:
         self.size = self.size(size)
         self.weight = self.setWeight()
 
+    def setWeight(self):
+        if self.type == "Carp":
+            return round(random.uniform(0.5, 4.0), 2)
+        if self.type == "Chub":
+            return round(random.uniform(0.5, 8.0), 2)
+        if self.type == "Zander":
+            return round(random.uniform(1.0, 11.0), 2)
+        if self.type == "Brown Trout":
+            return round(random.uniform(3.0, 20.0), 2)
+        if self.type == "Grayling":
+            return round(random.random(1.0, 6.7), 2)
+        if self.type == "Shoe fish":
+            return round(random.random())
+
     
     def size(self, size):
-        
         s = random.randint(size[0], size[1])
         return s
 
@@ -17,10 +30,10 @@ class Fish:
         prob = random.random()
 
         if prob < 0.5: return "Carp"
-        elif prob < 0.40: return "Goldfish"
-        elif prob < 0.35: return "Silverfish"
-        elif prob < 0.30: return "Crab"
-        elif prob < 0.25: return "Beluga"
+        elif prob < 0.40: return "Chub"
+        elif prob < 0.35: return "Zander"
+        elif prob < 0.30: return "Brown Trout"
+        elif prob < 0.25: return "Grayling"
         elif prob < 0.20: return "Shoe fish"
         elif prob < 0.15: return "Dead fish"
         elif prob < 0.10: return "Tiny fin"
